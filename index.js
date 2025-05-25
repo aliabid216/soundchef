8253fd042310f52286804f1518a7fbd0b42c522a
aliabid216 <122944230+aliabid216@users.noreply.github.com>
2025-05-25 16:18:51 +0400

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "100px", textAlign: "center", backgroundColor: "#0d0d0d", height: "100vh", color: "#fff" }}>
-      <h1 style={{ fontSize: "3em", color: "#ff6a00" }}>👨‍🍳 Welcome to SoundChef.io</h1>
-      <p>Your AI remix kitchen is heating up. The chef is preparing the next dish...</p>
-    </div>
-  );
-}
